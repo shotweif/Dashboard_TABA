0bd2a31a0ff34ada5e55edb343dd21babef49b89
Juan Posso <juan.posso@epn.edu.ec>
2024-09-23 10:54:51 -0500

    chore: remobve unused component

--- src/components/UsersTransactions.tsx
+++ src/components/UsersTransactions.tsx
@@ -4,23 +4,17 @@ import BarChartActiveUsers from './BarChartActiveUsers';
 
 interface UsersTransactionsProps {
     activeUsers: number;
-    inactiveUsers: number;
     totalTransactions: number;
-    historicalTotal: number;
 }
 
-const UsersTransactions: React.FC <UsersTransactionsProps> = ({activeUsers, totalTransactions, historicalTotal, inactiveUsers}) => {
+const UsersTransactions: React.FC <UsersTransactionsProps> = ({activeUsers, totalTransactions}) => {
     return (
-        <div>
-            
+        <div>          
              <h3 className="text-lg font-semibold text-secondary text-green-600">Usuarios Activos</h3>
-            <p className="text-4xl font-bold text-primary-foreground pb-10">{activeUsers.toLocaleString()}</p> 
+            <p className="text-4xl font-bold text-primary-foreground pb-10">{activeUsers}</p> 
             {/*<BarChartActiveUsers activeUsers={activeUsers} inactiveUsers={inactiveUsers} />*/}
-            <h3 className="text-lg font-semibold text-secondary text-green-600">Total Histórico</h3>
-            <p className="text-4xl font-bold text-primary-foreground">{historicalTotal.toLocaleString()}</p>
-            
-            <h3 className="text-lg font-semibold text-secondary text-green-600 pt-10">Total Diaria</h3>
-            <p className="text-4xl font-bold text-primary-foreground">{totalTransactions.toLocaleString()}</p>
+            <h3 className="text-lg font-semibold text-secondary text-green-600">Total de Transacciones</h3>
+            <p className="text-4xl font-bold text-primary-foreground">{totalTransactions}</p>
         </div>
     );
 };
