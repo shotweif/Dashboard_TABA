37043ce74cf457f5fe35625759fcd08c1b365376
Juan Posso <juan.posso@epn.edu.ec>
2024-10-04 12:57:19 -0500

    chore: delete unused functions

--- src/view/TransaccionesWEP.tsx
+++ src/view/TransaccionesWEP.tsx
@@ -4,7 +4,7 @@ import PieChart from "../components/PieChart";
 import DoughnutChart from "../components/DoughnutChart";
 import LineChart from "../components/LineChart";
 import UsersTransactions from "../components/UsersTransactions";
-import useFetchTransactions from "../hooks/useFectchFake";
+import useFetchTransactions from "../hooks/useFetchDataHook";
 import { getHours, parseISO, format, subDays, startOfDay } from "date-fns";
 import LoadingSpinner from "../components/LoadingSpinner";
 import { EmptyData } from "../components/EmptyData";
@@ -81,15 +81,32 @@ const TransaccionesWEP: React.FC<TransaccionesWEPProps> = ({
   return (
     <div className="p-4">
       {/* Line Chart */}
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4 "style={{ height: 'auto' }}>
-        <div className=" bg-card p-4 rounded-lg shadow hover:shadow-xl transition-shadow duration-300 col-span-3  bg-white  bg-white" style={{ borderLeft: '4px solid #78BE20', paddingLeft: '10px', height: '250px' }}>
+      <div
+        className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4 "
+        style={{ height: "auto" }}
+      >
+        <div
+          className=" bg-card p-4 rounded-lg shadow hover:shadow-xl transition-shadow duration-300 col-span-3  bg-white  bg-white"
+          style={{
+            borderLeft: "4px solid #78BE20",
+            paddingLeft: "10px",
+            height: "250px",
+          }}
+        >
           {allTransactions.length === 0 || emptyData ? (
             <EmptyData message="No existen datos de transacciones para las fechas seleccionadas" />
           ) : (
             <LineChart transaccionesTotales={counts} rangoTiempo={hours} />
           )}
         </div>
-        <div className=" bg-card p-4 rounded-lg shadow hover:shadow-xl transition-shadow duration-300 col-span-1  bg-white " style={{ borderLeft: '4px solid #78BE20', paddingLeft: '10px', height: '250px' }}>
+        <div
+          className=" bg-card p-4 rounded-lg shadow hover:shadow-xl transition-shadow duration-300 col-span-1  bg-white "
+          style={{
+            borderLeft: "4px solid #78BE20",
+            paddingLeft: "10px",
+            height: "250px",
+          }}
+        >
           <UsersTransactions
             activeUsers={activeUsers}
             totalTransactions={totalTransactions}
@@ -99,27 +116,35 @@ const TransaccionesWEP: React.FC<TransaccionesWEPProps> = ({
 
       {/* Other Charts */}
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4">
-      <div
-  className="bg-card p-4 rounded-lg shadow hover:shadow-xl transition-shadow duration-300 col-span-2  bg-white"
-  style={{
-    height: "387px", borderLeft: '4px solid #78BE20', paddingLeft: '10px', // Altura del contenedor principal
-  }}
->
-  {allTransactions.length === 0 || emptyData ? (
-    <EmptyData message="No existen datos de transacciones para las fechas seleccionadas" />
-  ) : (
-    <div className="h-full"> {/* Asegúrate de que el div use toda la altura disponible */}
-      <BarChart
-        transAceptada={receivedTransactions}
-        transRechazada={rejectedTransactions}
-      />
-    </div>
-  )}
-</div>
+        <div
+          className="bg-card p-4 rounded-lg shadow hover:shadow-xl transition-shadow duration-300 col-span-2  bg-white"
+          style={{
+            height: "387px",
+            borderLeft: "4px solid #78BE20",
+            paddingLeft: "10px", // Altura del contenedor principal
+          }}
+        >
+          {allTransactions.length === 0 || emptyData ? (
+            <EmptyData message="No existen datos de transacciones para las fechas seleccionadas" />
+          ) : (
+            <div className="h-full">
+              {" "}
+              {/* Asegúrate de que el div use toda la altura disponible */}
+              <BarChart
+                transAceptada={receivedTransactions}
+                transRechazada={rejectedTransactions}
+              />
+            </div>
+          )}
+        </div>
 
         <div
           className=" p-4 rounded-lg shadow hover:shadow-xl transition-shadow duration-300 col-span-1  bg-white "
-          style={{ height: "100%", borderLeft: '4px solid #78BE20', paddingLeft: '10px' }}
+          style={{
+            height: "100%",
+            borderLeft: "4px solid #78BE20",
+            paddingLeft: "10px",
+          }}
         >
           {allTransactions.length === 0 || emptyData ? (
             <EmptyData message="No existen datos de transacciones para las fechas seleccionadas" />
@@ -130,7 +155,10 @@ const TransaccionesWEP: React.FC<TransaccionesWEPProps> = ({
             />
           )}
         </div>
-        <div className=" bg-white p-4 rounded-lg shadow hover:shadow-xl transition-shadow duration-300 col-span-1  "style={{ borderLeft: '4px solid #78BE20', paddingLeft: '10px' }}>
+        <div
+          className=" bg-white p-4 rounded-lg shadow hover:shadow-xl transition-shadow duration-300 col-span-1  "
+          style={{ borderLeft: "4px solid #78BE20", paddingLeft: "10px" }}
+        >
           <DoughnutChart
             clientesActivos={activeUsers}
             clientesInactivos={inactiveUsers}
